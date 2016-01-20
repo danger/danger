3f6b15de736763f3c6f94a93f21f621b52d56688
Orta Therox <orta.therox@gmail.com>
2016-01-20 15:54:13 +0000

    Improve support on Circle CI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,18 @@
+## Master
+
+* Makes the CircleCI provider validate, but not run on non-PR builds - orta
+
+## 0.2.1
+
+* Edits an existing ticket rather than making a new one - orta
+
+## 0.2
+
+* Support making comments on a GitHub PR - Felix
+* Use GitHub status API to provide extra info on a PR - Felix
+* DRY the HTML comment - orta
+* Don't show a message if there are not warnings/errors - orta
+
 ## 0.1
 
 * Parses a `Dangerfile` - orta
--- lib/danger/ci_source/circle.rb
+++ lib/danger/ci_source/circle.rb
@@ -6,15 +6,16 @@ module Danger
     class CircleCI < CI
       def self.validates?(env)
         return !env["CIRCLE_BUILD_NUM"].nil? &&
-          !env["CI_PULL_REQUEST"].nil? &&
-          URI.parse(env["CI_PULL_REQUEST"]).path.split("/").count == 5
+               !env["CI_PULL_REQUEST"].nil?
       end
 
       def initialize(env)
-        paths = URI.parse(env["CI_PULL_REQUEST"]).path.split("/")
-        # The first one is an extra slash, ignore it
-        self.repo_slug = paths[1] + "/" + paths[2]
-        self.pull_request_id = paths[4]
+        if URI.parse(env["CI_PULL_REQUEST"]).path.split("/").count == 5
+          paths = URI.parse(env["CI_PULL_REQUEST"]).path.split("/")
+          # The first one is an extra slash, ignore it
+          self.repo_slug = paths[1] + "/" + paths[2]
+          self.pull_request_id = paths[4]
+        end
       end
     end
   end
