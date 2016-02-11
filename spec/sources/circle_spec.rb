5ac0ce7342e28bd79e2a95d6a8db007aa2fdb25b
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-02-11 00:57:39 -0800

    Fixing tests after rebasing

--- spec/sources/circle_spec.rb
+++ spec/sources/circle_spec.rb
@@ -59,18 +59,5 @@ describe Danger::CISource::CircleCI do
 
     expect(t.repo_slug).to eql("artsy/eigen")
     expect(t.pull_request_id).to eql("1130")
-    expect(t.base_commit).to eql("759adcbd0d8f")
-    expect(t.head_commit).to eql("13c4dc8bb61d")
-  end
-
-  it 'handles funky compare URLs' do
-    env = {
-      "CIRCLE_BUILD_NUM" => "true",
-      "CI_PULL_REQUEST" => "https://github.com/artsy/eigen/pull/800",
-      "CIRCLE_COMPARE_URL" => "https://github.com/artsy/eigen/compare/759adcbd0d8f^...13c4dc8bb61d"
-    }
-    t = Danger::CISource::CircleCI.new(env)
-    expect(t.base_commit).to eql("759adcbd0d8f^")
-    expect(t.head_commit).to eql("13c4dc8bb61d")
   end
 end
