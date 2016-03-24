fbbb47782f9dc6c1e16de56a583c535b3fda0c26
KrauseFx <KrauseFx@gmail.com>
2016-03-24 16:56:50 -0700

    Error handling for protect_files

--- lib/danger/plugins/protect_files.rb
+++ lib/danger/plugins/protect_files.rb
@@ -5,6 +5,9 @@ module Danger
       # be used in combination with some kind of
       # permission check if a user is inside the org
       def protect_files(path: nil, message: nil, fail_build: true)
+        raise "You have to provide a message" if message.to_s.length == 0
+        raise "You have to provide a path" if path.to_s.length == 0
+
         broken_rule = false
         Dir.glob(path) do |current|
           broken_rule = true if self.env.scm.modified_files.include?(current)
