4b9c7318aded372a361db9c79ca1549c03ba9d51
Anton Domashnev <antondomashnev@gmail.com>
2016-04-12 21:58:41 +0200

    Simplify regex for xcode_server ci source;

--- lib/danger/ci_source/xcode_server.rb
+++ lib/danger/ci_source/xcode_server.rb
@@ -11,9 +11,9 @@ module Danger
         bot_name = env["XCS_BOT_NAME"]
         return if bot_name.nil?
 
-        repo_matches = bot_name.match(/\[(.*?)\]/)
+        repo_matches = bot_name.match(/\[(.+)\]/)
         self.repo_slug = repo_matches[1] unless repo_matches.nil?
-        pull_request_id_matches = bot_name.match(/((?<=#)\d+)/)
+        pull_request_id_matches = bot_name.match(/#(\d+)/)
         self.pull_request_id = pull_request_id_matches[1] unless pull_request_id_matches.nil?
       end
     end
