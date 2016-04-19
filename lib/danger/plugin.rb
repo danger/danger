dbdbea41caf0b7e8bd6757856d0c2cfdf6de2af6
Samuel Giddins <segiddins@segiddins.me>
2016-04-18 19:53:08 -0500

    [Plugin] Clean up method_missing
    Object#send will call method_missing internally if necessary

--- lib/danger/plugin.rb
+++ lib/danger/plugin.rb
@@ -9,8 +9,7 @@ module Danger
         # Since we have a reference to the DSL containing all the information
         # We need to redirect the self calls to the DSL
         def method_missing(method_sym, *arguments, &_block)
-          return @dsl.send(method_sym, *arguments) if @dsl.respond_to?(method_sym)
-          return @dsl.method_missing(method_sym, *arguments)
+          @dsl.send(method_sym, *arguments)
         end
 
         def run
