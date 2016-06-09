5a0806050acc181e3ce956e1436f3ac5618e3f38
Roman Buussyghin <ramzes@yandex-team.ru>
2016-06-09 18:31:11 +0300

    Fix yet another test error

--- spec/lib/danger/ci_sources/teamcity_spec.rb
+++ spec/lib/danger/ci_sources/teamcity_spec.rb
@@ -9,18 +9,18 @@ describe Danger::CISource::TeamCity do
   it 'gets out a repo slug' do
     env = { 'GITHUB_REPO_SLUG' => 'foo/bar' }
     subject = Danger::CISource::TeamCity.new(env)
-    expect(t.repo_slug).to eql('foo/bar')
+    expect(subject.repo_slug).to eql('foo/bar')
   end
 
   it 'gets out a pull request id' do
     env = { 'GITHUB_PULL_REQUEST_ID' => '42' }
     subject = Danger::CISource::TeamCity.new(env)
-    expect(t.pull_request_id).to eql(42)
+    expect(subject.pull_request_id).to eql(42)
   end
 
   it 'gets out a repo url' do
     env = { 'GITHUB_REPO_URL' => 'http://example.org/' }
     subject = Danger::CISource::TeamCity.new(env)
-    expect(t.repo_url).to eql('http://example.org/')
+    expect(subject.repo_url).to eql('http://example.org/')
   end
 end
