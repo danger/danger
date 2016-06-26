37ba70ab8d249d38657a407f1011eb660c5c6446
Orta Therox <orta.therox@gmail.com>
2016-06-26 16:10:55 -0400

    Make green

--- lib/danger/commands/plugin_lint.rb
+++ lib/danger/commands/plugin_lint.rb
@@ -14,25 +14,25 @@ module Danger
     self.summary = 'Lint plugins from files, gems or the current folder. Outputs JSON array representation of Plugins on success.'
 
     self.description = <<-DESC
-      Converts a collection of file paths of Danger plugins into a JSON format. 
+      Converts a collection of file paths of Danger plugins into a JSON format.
       Note: Before 1.0, it will also parse the represented JSON to validate whether http://danger.systems would
       show the plugin on the website.
     DESC
 
     self.arguments = [
-      CLAide::Argument.new('Paths, Gems or Nothing', false, true),
+      CLAide::Argument.new('Paths, Gems or Nothing', false, true)
     ]
 
     def run
       paths = nil
 
       # When given existing paths, map to absolute & existing paths
-      if @refs != nil and @refs.select { |ref| File.file? ref }.any?
+      if !@refs.nil? and @refs.select { |ref| File.file? ref }.any?
         paths = @refs.select { |ref| File.file? ref }
                      .map { |path| File.expand_path(path) }
 
       # When given a list of gems
-      elsif @refs and @refs.is_a? Array 
+      elsif @refs and @refs.kind_of? Array
         require 'bundler'
         require 'pathname'
 
@@ -50,14 +50,14 @@ module Danger
           bundler.resolve_remotely!
 
           # Install the gems into a tmp dir
-          options = { :path => dir }
+          options = { path: dir }
           Bundler::Installer.install(Pathname.new(dir), bundler, options)
 
-          # Get the name'd gems out of bundler, then pull out all their paths 
+          # Get the name'd gems out of bundler, then pull out all their paths
           gems = gem_names.map { |name| bundler.specs[name] }.flatten
           paths = gems.map { |gem| Dir.glob(File.join(gem.gem_dir, "lib/**/**/**.rb")) }.flatten
         end
-      
+
       # When empty, imply you want to test the current lib folder as a plugin
       else
         paths = Dir.glob(File.join(".", "lib/*.rb"))
