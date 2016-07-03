00d8c833470f78cf9c3472d43d07c48918859c7c
Orta Therox <orta.therox@gmail.com>
2016-07-03 15:05:43 -0400

    Changes for danger systems

--- spec/lib/danger/request_sources/github_spec.rb
+++ spec/lib/danger/request_sources/github_spec.rb
@@ -85,21 +85,21 @@ describe Danger::RequestSources::GitHub do
       it "no warnings, no errors, no messages" do
         result = @g.generate_comment(warnings: [], errors: [], messages: [])
         expect(result.gsub(/\s+/, "")).to eq(
-          "<palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"https://github.com/danger/danger/\">danger</a></p>"
+          "<palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"http://danger.systems/\">danger</a></p>"
         )
       end
 
       it "supports markdown code below the summary table" do
         result = @g.generate_comment(warnings: violations(["ups"]), markdowns: ['### h3'])
         expect(result.gsub(/\s+/, "")).to eq(
-          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">1Warning</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">ups</td></tr></tbody></table>###h3<palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"https://github.com/danger/danger/\">danger</a></p>"
+          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">1Warning</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">ups</td></tr></tbody></table>###h3<palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"http://danger.systems/\">danger</a></p>"
         )
       end
 
       it "supports markdown only without a table" do
         result = @g.generate_comment(markdowns: ['### h3'])
         expect(result.gsub(/\s+/, "")).to eq(
-          "###h3<palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"https://github.com/danger/danger/\">danger</a></p>"
+          "###h3<palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"http://danger.systems/\">danger</a></p>"
         )
       end
 
@@ -107,7 +107,7 @@ describe Danger::RequestSources::GitHub do
         result = @g.generate_comment(warnings: violations(["my warning", "second warning"]), errors: [], messages: [])
         # rubocop:disable Metrics/LineLength
         expect(result.gsub(/\s+/, "")).to eq(
-          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">2Warnings</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">mywarning</td></tr><tr><td>:warning:</td><tddata-sticky=\"false\">secondwarning</td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"https://github.com/danger/danger/\">danger</a></p>"
+          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">2Warnings</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">mywarning</td></tr><tr><td>:warning:</td><tddata-sticky=\"false\">secondwarning</td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"http://danger.systems/\">danger</a></p>"
         )
         # rubocop:enable Metrics/LineLength
       end
@@ -117,7 +117,7 @@ describe Danger::RequestSources::GitHub do
         result = @g.generate_comment(warnings: warnings, errors: [], messages: [])
         # rubocop:disable Metrics/LineLength
         expect(result.gsub(/\s+/, "")).to eq(
-          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">2Warnings</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">amarkdown<ahref=\"https://github.com/danger/danger\">linktodanger</a></td></tr><tr><td>:warning:</td><tddata-sticky=\"false\">second<strong>warning</strong></td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"https://github.com/danger/danger/\">danger</a></p>"
+          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">2Warnings</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">amarkdown<ahref=\"https://github.com/danger/danger\">linktodanger</a></td></tr><tr><td>:warning:</td><tddata-sticky=\"false\">second<strong>warning</strong></td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"http://danger.systems/\">danger</a></p>"
         )
         # rubocop:enable Metrics/LineLength
       end
@@ -126,7 +126,7 @@ describe Danger::RequestSources::GitHub do
         result = @g.generate_comment(warnings: violations(["my warning"]), errors: violations(["some error"]), messages: [])
         # rubocop:disable Metrics/LineLength
         expect(result.gsub(/\s+/, "")).to eq(
-          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Error\">1Error</th></tr></thead><tbody><tr><td>:no_entry_sign:</td><tddata-sticky=\"false\">someerror</td></tr></tbody></table><table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">1Warning</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">mywarning</td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"https://github.com/danger/danger/\">danger</a></p>"
+          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Error\">1Error</th></tr></thead><tbody><tr><td>:no_entry_sign:</td><tddata-sticky=\"false\">someerror</td></tr></tbody></table><table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Warning\">1Warning</th></tr></thead><tbody><tr><td>:warning:</td><tddata-sticky=\"false\">mywarning</td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"http://danger.systems/\">danger</a></p>"
         )
         # rubocop:enable Metrics/LineLength
       end
@@ -154,7 +154,7 @@ describe Danger::RequestSources::GitHub do
 </table>
 
 <p align="right" data-meta="generated_by_danger">
-  Generated by :no_entry_sign: <a href="https://github.com/danger/danger/">danger</a>
+  Generated by :no_entry_sign: <a href="http://danger.systems/">danger</a>
 </p>
         HTML
       end
@@ -170,7 +170,7 @@ describe Danger::RequestSources::GitHub do
         previous_violations = { error: ['an error'] }
         result = @g.generate_comment(warnings: [], errors: violations(['an error']), messages: [], previous_violations: previous_violations)
         expect(result.gsub(/\s+/, "")).to eq(
-          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Error\">1Error</th></tr></thead><tbody><tr><td>:no_entry_sign:</td><tddata-sticky=\"false\">anerror</td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"https://github.com/danger/danger/\">danger</a></p>"
+          "<table><thead><tr><thwidth=\"50\"></th><thwidth=\"100%\"data-kind=\"Error\">1Error</th></tr></thead><tbody><tr><td>:no_entry_sign:</td><tddata-sticky=\"false\">anerror</td></tr></tbody></table><palign=\"right\"data-meta=\"generated_by_danger\">Generatedby:no_entry_sign:<ahref=\"http://danger.systems/\">danger</a></p>"
         )
       end
 
