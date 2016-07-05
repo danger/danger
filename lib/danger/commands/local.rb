d0f03524e77f92771aa76222caf6a7d2c844dda4
Michał Kałużny <maku@justmaku.org>
2016-07-05 13:12:08 +0200

    Use local branch names when calculating diff in local mode.

--- lib/danger/commands/local.rb
+++ lib/danger/commands/local.rb
@@ -63,7 +63,7 @@ module Danger
       begin
         dm.env.fill_environment_vars
         dm.env.ensure_danger_branches_are_setup
-        dm.env.scm.diff_for_folder('.', from: dm.env.ci_source.base_commit, to: dm.env.ci_source.head_commit)
+        dm.env.scm.diff_for_folder('.', from: Danger::EnvironmentManager.danger_base_branch, to: Danger::EnvironmentManager.danger_head_branch)
         dm.parse(Pathname.new(@dangerfile_path))
         dm.print_results
       ensure
