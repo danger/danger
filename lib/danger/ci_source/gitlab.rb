3a55e55920ac3f4e3eafa9800464aa4a93090ce6
Dean Perry <dean@voupe.com>
2016-08-06 23:21:14 +0100

    updated gitlab ci source. PR and MR currently aren’t set as env variables

--- lib/danger/ci_source/gitlab.rb
+++ lib/danger/ci_source/gitlab.rb
@@ -1,10 +1,10 @@
-# https://circleci.com/docs/environment-variables
+# http://docs.gitlab.com/ce/ci/variables/README.html
 require "uri"
 
 module Danger
   class GitlabCI < CI
     def self.validates_as_ci?(env)
-      env.key? "DRONE_REPO"
+      env.key? "GITLAB_CI"
     end
 
     def self.validates_as_pr?(env)
@@ -12,7 +12,7 @@ module Danger
     end
 
     def self.validates?(env)
-      return !env["GITLAB_PROJECT_ID"].nil? && !env["GITLAB_MR_ID"].nil?
+      return !env["CI_PROJECT_ID"].nil?# && !env["GITLAB_MR_ID"].nil?
     end
 
     def supported_request_sources
@@ -21,8 +21,8 @@ module Danger
 
     def initialize(env)
       # The first one is an extra slash, ignore it
-      self.repo_slug = env["GITLAB_PROJECT_ID"]
-      self.pull_request_id = env["GITLAB_MR_ID"]
+      self.repo_slug = env["CI_PROJECT_ID"]
+      # self.pull_request_id = env["GITLAB_MR_ID"]
     end
   end
 end
