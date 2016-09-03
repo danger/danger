2686ca0166abfb85fb955d6b8535e35110ebf101
JuanitoFatas <katehuang0320@gmail.com>
2016-09-04 00:34:12 +0800

    Use different command for windows & non-windows

--- spec/lib/danger/scm_source/git_repo_spec.rb
+++ spec/lib/danger/scm_source/git_repo_spec.rb
@@ -3,8 +3,9 @@ describe Danger::GitRepo do
     it "run command with our env set" do
       git_repo = described_class.new
       allow(git_repo).to receive(:default_env) { Hash("LANG" => "zh_TW.UTF-8") }
+      command = Gem.win_platform? ? "status && set LANG" : "status && echo $LANG"
 
-      result = git_repo.exec("status && echo $LANG")
+      result = git_repo.exec(command)
 
       expect(result).to match(/zh_TW.UTF-8/)
     end
