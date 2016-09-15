9cc1b8d601c0062e9eaf0dce9f21916d67b41f1c
JuanitoFatas <katehuang0320@gmail.com>
2016-09-15 23:45:21 +0800

    Add docs for String #danger_pluralize & #danger_underscore
    and Frozen ☃

--- lib/danger/core_ext/string.rb
+++ lib/danger/core_ext/string.rb
@@ -1,13 +1,15 @@
 class String
+  # @return [String] the plural form of self determined by count
   def danger_pluralize(count)
     "#{count} #{self}#{'s' unless count == 1}"
   end
 
+  # @return [String] converts to underscored, lowercase form
   def danger_underscore
-    self.gsub(/::/, "/").
-      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
-      gsub(/([a-z\d])([A-Z])/, '\1_\2').
-      tr("-", "_").
+    self.gsub(/::/, "/".freeze).
+      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2'.freeze).
+      gsub(/([a-z\d])([A-Z])/, '\1_\2'.freeze).
+      tr("-".freeze, "_".freeze).
       downcase
   end
 end
