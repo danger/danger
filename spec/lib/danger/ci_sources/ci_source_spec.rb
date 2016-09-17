27288a665d1939b12695185199e29fa191ebf631
JuanitoFatas <katehuang0320@gmail.com>
2016-09-18 01:11:33 +0800

    Fix danger/ci_source/ci_source spec

---
+++ spec/lib/danger/ci_sources/ci_source_spec.rb
@@ -0,0 +1,22 @@
+require "danger/ci_source/ci_source"
+
+describe Danger::CI do
+  describe ".available_ci_sources" do
+    it "returns list of CI subclasses" do
+      expect(described_class.available_ci_sources.map(&:to_s)).to match_array([
+        "Danger::LocalGitRepo",
+        "Danger::Bitrise",
+        "Danger::Buildkite",
+        "Danger::CircleCI",
+        "Danger::Drone",
+        "Danger::GitLabCI",
+        "Danger::Jenkins",
+        "Danger::Semaphore",
+        "Danger::Surf",
+        "Danger::TeamCity",
+        "Danger::Travis",
+        "Danger::XcodeServer"
+      ])
+    end
+  end
+end
