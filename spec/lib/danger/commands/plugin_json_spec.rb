8e0acc56d9f5220d53a4df8dcfe43cb1c29f8d99
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2016-10-06 21:43:35 +0200

    remove stout output in spec and be more specific

--- spec/lib/danger/commands/plugin_json_spec.rb
+++ spec/lib/danger/commands/plugin_json_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe Danger::PluginJSON do
     Danger::Plugin.clear_external_plugins
   end
 
-  it "runs the command" do
-    described_class.run(["spec/fixtures/plugins/example_fully_documented.rb"])
+  it "outputs a plugins documentation as json" do
+    expect do
+      described_class.run(["spec/fixtures/plugins/example_fully_documented.rb"])
+    end.to output(/DangerProselint/).to_stdout
   end
 end
