d76fba8205f8ab92c259ff480b5822acc9cafd95
Juanito Fatas <katehuang0320@gmail.com>
2016-10-08 01:27:53 +0800

    Improve the matcher for showing actual value

--- spec/support/matchers/have_instance_variables_matcher.rb
+++ spec/support/matchers/have_instance_variables_matcher.rb
@@ -9,7 +9,7 @@ RSpec::Matchers.define(:have_instance_variables) do |expected|
     expected.each do |instance_variable, expected_value|
       actual_value = actual.instance_variable_get(instance_variable)
       if actual_value != expected_value
-        return "expected #{actual}#{instance_variable} to match #{expected_value}, but got #{actual_value}."
+        return "expected #{actual}#{instance_variable} to match #{expected_value.inspect}, but got #{actual_value.inspect}."
       end
     end
   end
@@ -18,7 +18,7 @@ RSpec::Matchers.define(:have_instance_variables) do |expected|
     expected.each do |instance_variable, expected_value|
       actual_value = actual.instance_variable_get(instance_variable)
       if actual_value == expected_value
-        return "expected #{actual}#{instance_variable} not to match #{expected_value}, but got #{actual_value}."
+        return "expected #{actual}#{instance_variable} not to match #{expected_value.inspect}, but got #{actual_value.inspect}."
       end
     end
   end
