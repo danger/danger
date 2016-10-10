95a32c52de0d220c18b0e152697cfb2a78e96a7c
Juanito Fatas <katehuang0320@gmail.com>
2016-10-11 06:19:37 +0800

    Use `git-merge-base` instead of ruby-git to find more merge bases
    OPTIONS
           -a, --all
               Output all merge bases for the commits, instead of just one.
    
    Fail fast if we cannot find merge base

--- spec/lib/danger/request_sources/github_spec.rb
+++ spec/lib/danger/request_sources/github_spec.rb
@@ -341,14 +341,14 @@ RSpec.describe Danger::RequestSources::GitHub, host: :github do
       it "setups the danger branches" do
         @g.fetch_details
         expect(@g.scm).to receive(:exec)
-          .with("rev-parse --quiet --verify \"704dc55988c6996f69b6873c2424be7d1de67bbe^{commit}\"")
+          .with("rev-parse --quiet --verify 704dc55988c6996f69b6873c2424be7d1de67bbe^{commit}")
           .and_return("345e74fabb2fecea93091e8925b1a7a208b48ba6").twice
 
         expect(@g.scm).to receive(:exec)
           .with("branch danger_base 704dc55988c6996f69b6873c2424be7d1de67bbe")
 
         expect(@g.scm).to receive(:exec)
-          .with("rev-parse --quiet --verify \"561827e46167077b5e53515b4b7349b8ae04610b^{commit}\"")
+          .with("rev-parse --quiet --verify 561827e46167077b5e53515b4b7349b8ae04610b^{commit}")
           .and_return("561827e46167077b5e53515b4b7349b8ae04610b").twice
 
         expect(@g.scm).to receive(:exec)
@@ -360,13 +360,13 @@ RSpec.describe Danger::RequestSources::GitHub, host: :github do
       it "fetches when the branches are not in the local store" do
         # not in history
         expect(@g.scm).to receive(:exec).
-          with("rev-parse --quiet --verify \"704dc55988c6996f69b6873c2424be7d1de67bbe^{commit}\"").
+          with("rev-parse --quiet --verify 704dc55988c6996f69b6873c2424be7d1de67bbe^{commit}").
           and_return("")
         # fetch it
         expect(@g.scm).to receive(:exec).with("fetch")
         # still not in history
         expect(@g.scm).to receive(:exec).
-          with("rev-parse --quiet --verify \"704dc55988c6996f69b6873c2424be7d1de67bbe^{commit}\"").
+          with("rev-parse --quiet --verify 704dc55988c6996f69b6873c2424be7d1de67bbe^{commit}").
           and_return("")
 
         expect do
