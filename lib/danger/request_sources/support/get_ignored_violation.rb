281c764f3df5d28dee0d1e664a4699db016e9adc
Juanito Fatas <katehuang0320@gmail.com>
2016-10-26 16:05:31 +0800

    Improves GetIgnoredViolation Regexp

--- lib/danger/request_sources/support/get_ignored_violation.rb
+++ lib/danger/request_sources/support/get_ignored_violation.rb
@@ -1,5 +1,5 @@
 class GetIgnoredViolation
-  IGNORE_REGEXP = />*\s*danger\s*:\s*ignore\s*"(.*)"/i
+  IGNORE_REGEXP = %r(>*\s*danger\s*:\s*ignore\s*"(?<error>[^"]*)")i
 
   def initialize(body)
     @body = body
