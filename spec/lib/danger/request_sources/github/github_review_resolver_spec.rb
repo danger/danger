6e612c6b0006973aeaecf7e0bd2b68f74234ab1a
Anton Domashnev <antondomashnev@gmail.com>
2017-01-20 23:52:09 +0100

    Create ReviewResolver tests;

--- spec/lib/danger/request_sources/github/github_review_resolver_spec.rb
+++ spec/lib/danger/request_sources/github/github_review_resolver_spec.rb
@@ -1,117 +1,156 @@
 require "danger/request_sources/github/github_review_resolver"
 require "danger/request_sources/github/github_review"
 
-RSpec.describe Danger::GitHub::ReviewResolver do
+RSpec.describe Danger::RequestSources::GitHubSource::ReviewResolver do
   let(:review) { double("Danger::GitHub::Review") }
-  subject { Danger::GitHub::ReviewResolver.new(review) }
 
   describe "should_submit?" do
-    context "when review status is pending" do
+    context "when submission body the same as review has" do
       before do
-        allow(review).to receive(:status) { Danger::GitHub::Review::STATUS_PENDING }
+        allow(review).to receive(:body).and_return "super body"
       end
 
-      context "when wants to approve" do
-        it "returns false" do
-          expect(subject.should_submit?(Danger::GitHub::Review::EVENT_APPROVE)).to be false
-        end
-      end
-
-      context "when wants to request changes" do
-        it "returns false" do
-          expect(subject.should_submit?(Danger::GitHub::Review::EVENT_REQUEST_CHANGES)).to be false
-        end
+      it "returns false" do
+        expect(described_class.should_submit?(review, Danger::RequestSources::GitHubSource::Review::EVENT_APPROVE, "super body").to be false
       end
     end
 
-    context "when review status is requested changes" do
+    context "when submission body is different to review body" do
+      let (:submissions_body) { "submission body" }
+
       before do
-        allow(review).to receive(:status) { Danger::GitHub::Review::STATUS_REQUESTED_CHANGES }
+        allow(review).to receive(:body).and_return "unique body"
       end
 
-      context "when wants to approve" do
-        it "returns true" do
-          expect(subject.should_submit?(Danger::GitHub::Review::EVENT_APPROVE)).to be true
+      context "when want to approve pr" do
+        let (:submission_event) { Danger::RequestSources::GitHubSource::Review::EVENT_APPROVE }
+
+        context "when review is pending" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_PENDING }
+          end
+
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
+        end
+
+        context "when review is approved" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_APPROVED }
+          end
+
+          it "returns false" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be false
+          end
+        end
+
+        context "when review has request changes" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_REQUESTED_CHANGES }
+          end
+
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
+        end
+
+        context "when review is commented" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_COMMENTED }
+          end
+
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
         end
       end
 
-      context "when wants to request changes" do
-        it "returns true" do
-          expect(subject.should_submit?(Danger::GitHub::Review::EVENT_REQUEST_CHANGES)).to be true
+      context "when want to request changes for pr" do
+        let (:submission_event) { Danger::RequestSources::GitHubSource::Review::EVENT_REQUEST_CHANGES }
+
+        context "when review is pending" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_PENDING }
+          end
+
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
         end
-      end
-    end
 
-    context "when review status is approved" do
-      before do
-        allow(review).to receive(:status) { Danger::GitHub::Review::STATUS_APPROVED }
-      end
+        context "when review is approved" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_APPROVED }
+          end
 
-      context "when wants to approve" do
-        it "returns false" do
-          expect(subject.should_submit?(Danger::GitHub::Review::EVENT_APPROVE)).to be false
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
         end
-      end
 
-      context "when wants to request changes" do
-        it "returns false" do
-          expect(subject.should_submit?(Danger::GitHub::Review::EVENT_REQUEST_CHANGES)).to be false
+        context "when review has request changes" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_REQUESTED_CHANGES }
+          end
+
+          it "returns false" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be false
+          end
         end
-      end
-    end
-  end
 
-  describe "should_create?" do
-    context "when review status is pending" do
-      before do
-        allow(review).to receive(:status) { Danger::GitHub::Review::STATUS_PENDING }
-      end
+        context "when review is commented" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_COMMENTED }
+          end
 
-      context "when wants to approve" do
-        it "returns true" do
-          expect(subject.should_create?(Danger::GitHub::Review::EVENT_APPROVE)).to be true
-        end
-      end
-
-      context "when wants to request changes" do
-        it "returns true" do
-          expect(subject.should_create?(Danger::GitHub::Review::EVENT_REQUEST_CHANGES)).to be true
-        end
-      end
-    end
-
-    context "when review status is requested changes" do
-      before do
-        allow(review).to receive(:status) { Danger::GitHub::Review::STATUS_REQUESTED_CHANGES }
-      end
-
-      context "when wants to approve" do
-        it "returns false" do
-          expect(subject.should_create?(Danger::GitHub::Review::EVENT_APPROVE)).to be false
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
         end
       end
 
-      context "when wants to request changes" do
-        it "returns true" do
-          expect(subject.should_create?(Danger::GitHub::Review::EVENT_REQUEST_CHANGES)).to be false
+      context "when want to comment pr" do
+        let (:submission_event) { Danger::RequestSources::GitHubSource::Review::EVENT_COMMENT }
+
+        context "when review is pending" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_PENDING }
+          end
+
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
         end
-      end
-    end
 
-    context "when review status is approved" do
-      before do
-        allow(review).to receive(:status) { Danger::GitHub::Review::STATUS_APPROVED }
-      end
+        context "when review is approved" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_APPROVED }
+          end
 
-      context "when wants to approve" do
-        it "returns false" do
-          expect(subject.should_create?(Danger::GitHub::Review::EVENT_APPROVE)).to be false
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
         end
-      end
 
-      context "when wants to request changes" do
-        it "returns true" do
-          expect(subject.should_create?(Danger::GitHub::Review::EVENT_REQUEST_CHANGES)).to be true
+        context "when review has request changes" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_REQUESTED_CHANGES }
+          end
+
+          it "returns true" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be true
+          end
+        end
+
+        context "when review is commented" do
+          before do
+            allow(review).to receive(:status) { Danger::RequestSources::GitHubSource::Review::STATUS_COMMENTED }
+          end
+
+          it "returns false" do
+            expect(described_class.should_submit?(review, submission_event, submissions_body)).to be false
+          end
         end
       end
     end
