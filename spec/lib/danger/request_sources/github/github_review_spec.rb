b5bd8f62d14d374ea522512004d8ee640e6df3d3
Anton Domashnev <antondomashnev@gmail.com>
2017-01-22 12:09:59 +0100

    Finalize spec for github review;

--- spec/lib/danger/request_sources/github/github_review_spec.rb
+++ spec/lib/danger/request_sources/github/github_review_spec.rb
@@ -1,6 +1,10 @@
 require "danger/request_sources/github/github_review"
+require "danger/helpers/comments_helper"
+require "danger/helpers/comment"
 
 RSpec.describe Danger::RequestSources::GitHubSource::Review, host: :github do
+  include Danger::Helpers::CommentsHelper
+
   let(:client) { double(Octokit::Client) }
 
   describe "submit" do
@@ -8,9 +12,34 @@ RSpec.describe Danger::RequestSources::GitHubSource::Review, host: :github do
       Danger::RequestSources::GitHubSource::Review.new(client, stub_ci)
     end
 
-    context "when there are messages" do
+    before do
+      subject.start
+    end
+
+    it "submits review request with correct body" do
+      subject.message("Hi")
+      subject.markdown("Yo")
+      subject.warn("I warn you")
+      subject.fail("This is bad, really bad")
+
+      messages = [Danger::Violation.new("Hi", true)]
+      warnings = [Danger::Violation.new("I warn you", true)]
+      errors = [Danger::Violation.new("This is bad, really bad", true)]
+      markdowns = [Danger::Markdown.new("Yo", true), Danger::Markdown.new(generate_description(warnings: warnings, errors: errors), true)]
+      expected_body = generate_comment(warnings: warnings,
+                                       errors: errors,
+                                       messages: messages,
+                                       markdowns: markdowns,
+                                       previous_violations: parse_comment(""),
+                                       danger_id: "danger",
+                                       template: "github")
+
+      expect(client).to receive(:create_pull_request_review).with(stub_ci.repo_slug, stub_ci.pull_request_id, Danger::RequestSources::GitHubSource::Review::EVENT_REQUEST_CHANGES, expected_body)
+      subject.submit
+    end
+
+    context "when there are only messages" do
       before do
-        subject.start
         subject.message("Hi")
       end
 
@@ -20,8 +49,64 @@ RSpec.describe Danger::RequestSources::GitHubSource::Review, host: :github do
       end
     end
 
-    it "suggests changes to the pr" do
-      allow(client).to receive(:create_pull_request_review).with(stub_ci.repo_slug, stub_ci.pull_request_id, )
+    context "when there are only markdowns" do
+      before do
+        subject.markdown("Hi")
+      end
+
+      it "approves the pr" do
+        expect(client).to receive(:create_pull_request_review).with(stub_ci.repo_slug, stub_ci.pull_request_id, Danger::RequestSources::GitHubSource::Review::EVENT_APPROVE, anything)
+        subject.submit
+      end
+    end
+
+    context "when there are only warnings" do
+      before do
+        subject.warn("Hi")
+      end
+
+      it "approves the pr" do
+        expect(client).to receive(:create_pull_request_review).with(stub_ci.repo_slug, stub_ci.pull_request_id, Danger::RequestSources::GitHubSource::Review::EVENT_APPROVE, anything)
+        subject.submit
+      end
+    end
+
+    context "when there are only errors" do
+      before do
+        subject.fail("Yo")
+      end
+
+      it "suggests changes to the pr" do
+        expect(client).to receive(:create_pull_request_review).with(stub_ci.repo_slug, stub_ci.pull_request_id, Danger::RequestSources::GitHubSource::Review::EVENT_REQUEST_CHANGES, anything)
+        subject.submit
+      end
+    end
+
+    context "when there are errors" do
+      before do
+        subject.message("Hi")
+        subject.markdown("Yo")
+        subject.warn("I warn you")
+        subject.fail("This is bad, really bad")
+      end
+
+      it "suggests changes to the pr" do
+        expect(client).to receive(:create_pull_request_review).with(stub_ci.repo_slug, stub_ci.pull_request_id, Danger::RequestSources::GitHubSource::Review::EVENT_REQUEST_CHANGES, anything)
+        subject.submit
+      end
+    end
+
+    context "when there are no errors" do
+      before do
+        subject.message("Hi")
+        subject.markdown("Yo")
+        subject.warn("I warn you")
+      end
+
+      it "sapproves the pr" do
+        expect(client).to receive(:create_pull_request_review).with(stub_ci.repo_slug, stub_ci.pull_request_id, Danger::RequestSources::GitHubSource::Review::EVENT_APPROVE, anything)
+        subject.submit
+      end
     end
   end
 
