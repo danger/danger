a01b1e95f026d26bb82d4792ac23ce016dc896ec
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-02-10 21:01:49 +0100

    Extract class to peform dangerfile evaluation

--- lib/danger/commands/local.rb
+++ lib/danger/commands/local.rb
@@ -101,15 +101,28 @@ module Danger
 
       dm.env.request_source = gh
 
-      begin
-        dm.env.fill_environment_vars
-        dm.env.ensure_danger_branches_are_setup
-        dm.env.scm.diff_for_folder(".", from: Danger::EnvironmentManager.danger_base_branch, to: Danger::EnvironmentManager.danger_head_branch)
+      DangerfileLocalEvaluator
+        .new(dm, Pathname.new(@dangerfile_path))
+        .evaluate
+    end
+  end
 
-        dm.parse(Pathname.new(@dangerfile_path))
-        dm.print_results
+  class DangerfileLocalEvaluator
+    def initialize(dm, dangerfile_path)
+      @dangerfile_path = dangerfile_path
+      @dm = dm
+    end
+
+    def evaluate
+      begin
+        @dm.env.fill_environment_vars
+        @dm.env.ensure_danger_branches_are_setup
+        @dm.env.scm.diff_for_folder(".", from: Danger::EnvironmentManager.danger_base_branch, to: Danger::EnvironmentManager.danger_head_branch)
+
+        @dm.parse(@dangerfile_path)
+        @dm.print_results
       ensure
-        dm.env.clean_up
+        @dm.env.clean_up
       end
     end
   end
