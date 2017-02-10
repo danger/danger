fad95c2434ce3d8688bc0ad6f4ce43490fc5e007
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-02-10 22:42:27 +0100

    Explicit `begin` not needed

--- lib/danger/commands/local_helpers/dangerfile_evaluator.rb
+++ lib/danger/commands/local_helpers/dangerfile_evaluator.rb
@@ -6,16 +6,14 @@ module Danger
     end
 
     def evaluate
-      begin
-        @dm.env.fill_environment_vars
-        @dm.env.ensure_danger_branches_are_setup
-        @dm.env.scm.diff_for_folder(".", from: Danger::EnvironmentManager.danger_base_branch, to: Danger::EnvironmentManager.danger_head_branch)
+      @dm.env.fill_environment_vars
+      @dm.env.ensure_danger_branches_are_setup
+      @dm.env.scm.diff_for_folder(".", from: Danger::EnvironmentManager.danger_base_branch, to: Danger::EnvironmentManager.danger_head_branch)
 
-        @dm.parse(@dangerfile_path)
-        @dm.print_results
-      ensure
-        @dm.env.clean_up
-      end
+      @dm.parse(@dangerfile_path)
+      @dm.print_results
+    ensure
+      @dm.env.clean_up
     end
   end
 end
