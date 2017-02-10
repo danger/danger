0d9f1d98c171f150822186eb714996bc5416fd51
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-02-10 21:16:30 +0100

    Ruby has the beautiful unless keyword

--- lib/danger/commands/local.rb
+++ lib/danger/commands/local.rb
@@ -77,7 +77,7 @@ module Danger
 
       cork.puts "Running your Dangerfile against this PR - https://#{gh.host}/#{source.repo_slug}/pull/#{source.pull_request_id}"
 
-      if verbose != true
+      unless verbose
         cork.puts "Turning on --verbose"
         dm.verbose = true
       end
