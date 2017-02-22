aebceea62cb5e44a1ba8d49c50514d8b439b33da
Juanito Fatas <juanito-fatas@cookpad.jp>
2017-02-23 02:30:37 +0900

    Fix build: slug of gitlab url no need to be escaped now and use released gitlab 3.7.0

--- spec/support/gitlab_helper.rb
+++ spec/support/gitlab_helper.rb
@@ -27,29 +27,25 @@ module Danger
 
       def stub_merge_request(fixture, slug, merge_request_id)
         raw_file = File.new("spec/fixtures/gitlab_api/#{fixture}.json")
-        escaped_slug = CGI.escape(slug)
-        url = "https://gitlab.com/api/v3/projects/#{escaped_slug}/merge_request/#{merge_request_id}"
+        url = "https://gitlab.com/api/v3/projects/#{slug}/merge_request/#{merge_request_id}"
         WebMock.stub_request(:get, url).with(headers: expected_headers).to_return(raw_file)
       end
 
       def stub_merge_request_changes(fixture, slug, merge_request_id)
         raw_file = File.new("spec/fixtures/gitlab_api/#{fixture}.json")
-        escaped_slug = CGI.escape(slug)
-        url = "https://gitlab.com/api/v3/projects/#{escaped_slug}/merge_request/#{merge_request_id}/changes"
+        url = "https://gitlab.com/api/v3/projects/#{slug}/merge_request/#{merge_request_id}/changes"
         WebMock.stub_request(:get, url).with(headers: expected_headers).to_return(raw_file)
       end
 
       def stub_merge_request_commits(fixture, slug, merge_request_id)
         raw_file = File.new("spec/fixtures/gitlab_api/#{fixture}.json")
-        escaped_slug = CGI.escape(slug)
-        url = "https://gitlab.com/api/v3/projects/#{escaped_slug}/merge_request/#{merge_request_id}/commits"
+        url = "https://gitlab.com/api/v3/projects/#{slug}/merge_request/#{merge_request_id}/commits"
         WebMock.stub_request(:get, url).with(headers: expected_headers).to_return(raw_file)
       end
 
       def stub_merge_request_comments(fixture, slug, merge_request_id)
         raw_file = File.new("spec/fixtures/gitlab_api/#{fixture}.json")
-        escaped_slug = CGI.escape(slug)
-        url = "https://gitlab.com/api/v3/projects/#{escaped_slug}/merge_requests/#{merge_request_id}/notes"
+        url = "https://gitlab.com/api/v3/projects/#{slug}/merge_requests/#{merge_request_id}/notes"
         WebMock.stub_request(:get, url).with(headers: expected_headers).to_return(raw_file)
       end
     end
