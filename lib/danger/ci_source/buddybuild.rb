d5ec5f94ca6311743bf2281496a7dcac6e55bdd9
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-03-19 02:38:07 +0100

    Add CI description

--- lib/danger/ci_source/buddybuild.rb
+++ lib/danger/ci_source/buddybuild.rb
@@ -1,6 +1,22 @@
 module Danger
   # ### CI Setup
+  #
+  # Buddybuild has an integration for Danger already built-in. 
+  # What you need to do is to upload your `Gemfile` and `Dangerfile` to 
+  # the server and you should be all set-up. However, if you want to use 
+  # different bot for Danger, you can do it with token setup described below.
+  #
   # ### Token Setup
+  #
+  # Login to buddybuild and select your app. Go to your *App Settings* and 
+  # in the *Build Settings* menu on the left, choose *Environment Variables*. 
+  #
+  # #### GitHub
+  # Add the `DANGER_GITHUB_API_TOKEN` to your build user's ENV.
+  #
+  # #### GitLab
+  # Add the `DANGER_GITLAB_API_TOKEN` to your build user's ENV.
+
   class Buddybuild < CI
     def self.validates_as_ci?(env)
       false
