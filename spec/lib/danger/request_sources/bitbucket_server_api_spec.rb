1821d4ffd5d08218969dabda98ab963d8e3af082
Rishabh Tayal <rtayal@ushofml9k57.kih.kmart.com>
2017-04-17 13:45:49 -0500

    added test case

--- spec/lib/danger/request_sources/bitbucket_server_api_spec.rb
+++ spec/lib/danger/request_sources/bitbucket_server_api_spec.rb
@@ -10,5 +10,15 @@ RSpec.describe Danger::RequestSources::BitbucketServerAPI, host: :bitbucket_serv
 
       expect(inspected).to include(%(@password="********"))
     end
+
+    it "handles http hosts" do
+      env = stub_env
+      env["DANGER_BITBUCKETSERVER_HOST"] = "http://my_url..."
+      api = described_class.new("danger", "danger", 1, env)
+      expect(api.pr_api_endpoint).to.equal("http://my_url/things/pull-requests/1")
+      env["DANGER_BITBUCKETSERVER_HOST"] = "my_url..."
+      api = described_class.new("danger", "danger", 1, env)
+      expect(api.pr_api_endpoint).to.equal("https://my_url/things/pull-requests/1")
+   end
   end
 end
