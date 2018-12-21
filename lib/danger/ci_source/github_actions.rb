358e1bfd42e46e8c16f896268df54e3be404fa0b
shunsuke maeda <duck8823@gmail.com>
2018-12-21 22:02:54 +0900

    Add github actions

---
+++ lib/danger/ci_source/github_actions.rb
@@ -0,0 +1,24 @@
+require "danger/request_sources/github/github"
+
+module Danger
+  class GitHubActions < CI
+    def self.validates_as_ci?(env)
+      env.key? "GITHUB_ACTION"
+    end
+
+    def self.validates_as_pr?(env)
+      env["GITHUB_EVENT_NAME"] == "pull_request"
+    end
+
+    def supported_request_sources
+      @supported_request_sources ||= [Danger::RequestSources::GitHub]
+    end
+
+    def initialize(env)
+      self.repo_slug = env["GITHUB_REPOSITORY"]
+      pull_request_event = JSON.parse(File.read(env["GITHUB_EVENT_PATH"]))
+      self.pull_request_id = pull_request_event['number']
+      self.repo_url = pull_request_event['repository']['clone_url']
+    end
+  end
+end
