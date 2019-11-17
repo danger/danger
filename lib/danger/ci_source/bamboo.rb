994f83ecc9da091fc38a73bd867eac36073b1fc1
Henryk Ratajczak <henryk.ratajczak@allegro.pl>
2019-11-17 15:48:45 +0100

    Improve documentation.

--- lib/danger/ci_source/bamboo.rb
+++ lib/danger/ci_source/bamboo.rb
@@ -3,17 +3,17 @@ require "set"
 module Danger
   # ### CI Setup
   #
-  # Add `bundle exec danger` as Run Script step inside your plan. XXX expand me.
+  # Add a Run Script task that executes `danger` (or `bundle exec danger` if you're using Bundler
+  # to manage your gems) as your as part of your Bamboo plan.
+  # The minimum supported version is Bamboo 6.9.
   #
   # ### Token Setup
   #
-  # Bamboo will make available all required environment variables only if the plan is ran as part of
-  # a pull request. Otherwise, `bamboo_repository_pr_key` and `bamboo_planRepository_repositoryUrl`
-  # variables will not be available.
-  #
-  # #### Bitbucket Server integration
-  # Make sure `DANGER_BITBUCKETSERVER_USERNAME`, `DANGER_BITBUCKETSERVER_PASSWORD`
-  # and `DANGER_BITBUCKETSERVER_HOST` ENV variables are all available by XXX.
+  # IMPORTANT: All required Bamboo environment variables will be available
+  # only if the plan is run as part of a pull request. This can be achieved by selecting:
+  # Configure plan -> Branches -> Create plan branch: "When pull request is created".
+  # Otherwise, `bamboo_repository_pr_key` and `bamboo_planRepository_repositoryUrl`
+  # will not be available.
   #
   class Bamboo < CI
     def supported_request_sources
@@ -32,7 +32,6 @@ module Danger
     end
 
     def initialize(env)
-      # TODO: What if there's more than one?
       self.repo_url = env["bamboo_planRepository_repositoryUrl"]
       self.pull_request_id = env["bamboo_repository_pr_key"]
       repo_matches = self.repo_url.match(%r{([\/:])([^\/]+\/[^\/]+?)(\.git$|$)})
