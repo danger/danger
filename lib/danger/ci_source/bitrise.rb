7bc0448a094678af2d3d3ff053b61c53a6198102
RonaldPrithiv <32409931+RonaldPrithiv@users.noreply.github.com>
2020-02-17 10:01:18 +0400

    Update bitrise.rb

--- lib/danger/ci_source/bitrise.rb
+++ lib/danger/ci_source/bitrise.rb
@@ -25,7 +25,7 @@ module Danger
   # finding the project and repo slug in the GIT_REPOSITORY_URL variable. This GIT_REPOSITORY_URL variable 
   # comes from the App Settings tab for your Bitrsie App. If you are manually setting a repo URL in the 
   # Git Clone Repo step, you may need to set adjust this propery in the settings tab, maybe even fake it.
-  # The pattern used is `(%r{([\/:])(([^\/]+\/)+[^\/]+?)(\.git$|$)}`.
+  # The patterns used are `(%r{\.com/(.*)})` and `(%r{\.com:(.*)})` .
   #
   class Bitrise < CI
     def self.validates_as_ci?(env)
