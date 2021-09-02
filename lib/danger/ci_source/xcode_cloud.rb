5a4cf63b382ab46562e24aa4f0583e63434fdb61
Rogerio de Paula Assis <rogerio.assis@vimeo.com>
2021-09-02 19:16:20 +1000

    Adds Xcode Cloud support to danger

---
+++ lib/danger/ci_source/xcode_cloud.rb
@@ -0,0 +1,23 @@
+require "danger/request_sources/github/github"
+
+module Danger
+  class XcodeCloud < CI
+    def self.validates_as_ci?(env)
+      env.key? "CI_XCODEBUILD_ACTION"
+    end
+
+    def self.validates_as_pr?(env)
+      env.key? "CI_PULL_REQUEST_NUMBER"
+    end
+
+    def supported_request_sources
+      @supported_request_sources ||= [Danger::RequestSources::GitHub]
+    end
+
+    def initialize(env)
+      self.repo_slug = env["CI_PULL_REQUEST_SOURCE_REPO"]
+      self.pull_request_id = env["CI_PULL_REQUEST_NUMBER"]
+      self.repo_url = env["CI_PULL_REQUEST_HTML_URL"]
+    end
+  end
+end
