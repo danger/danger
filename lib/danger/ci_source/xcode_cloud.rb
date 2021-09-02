213030c2456c297293f27520e2581e5364d19eb1
Rogerio de Paula Assis <rogerio.assis@vimeo.com>
2021-09-02 20:29:37 +1000

    Adds supported requested sources

--- lib/danger/ci_source/xcode_cloud.rb
+++ lib/danger/ci_source/xcode_cloud.rb
@@ -11,7 +11,12 @@ module Danger
     end
 
     def supported_request_sources
-      @supported_request_sources ||= [Danger::RequestSources::GitHub]
+      @supported_request_sources ||= [
+        Danger::RequestSources::GitHub,
+        Danger::RequestSources::GitLab, 
+        Danger::RequestSources::BitbucketCloud, 
+        Danger::RequestSources::BitbucketServer
+      ]
     end
 
     def initialize(env)
