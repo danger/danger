1a958bcd1c32320d450f09a69ef9ae0200e28495
Mustafa <971530+tosbaha@users.noreply.github.com>
2022-08-24 14:26:20 +0300

    Added help links

--- lib/danger/ci_source/appcircle.rb
+++ lib/danger/ci_source/appcircle.rb
@@ -1,4 +1,5 @@
-# https://www.appveyor.com/docs/build-configuration/
+# https://docs.appcircle.io/environment-variables/managing-variables
+# https://docs.appcircle.io/build/build-profile-configuration#environment-variables-configuration
 require "danger/request_sources/github/github"
 require "danger/request_sources/gitlab"
 module Danger
